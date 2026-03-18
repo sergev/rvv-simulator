ef97adf77f7fcfbb55d2fda7c04f7073dd6b9374
Serge Vakulenko <serge.vakulenko@gmail.com>
2026-03-17 23:02:51 -0700

    Simplify stdlib tests.

--- tests/stdlib.cpp
+++ tests/stdlib.cpp
@@ -16,18 +16,29 @@
 #include <vector>
 
 namespace {
-    std::default_random_engine generator;
+
+constexpr size_t kBufSize = 1024;
+using buf_type = std::vector<char>;
+
+std::default_random_engine& GetGenerator() {
+    static std::default_random_engine generator;
+    return generator;
+}
+
+buf_type RandomBuf(size_t n = kBufSize) {
+    buf_type buf;
+    buf.reserve(n);
+    std::uniform_int_distribution<int> distribution(0, 255);
+    auto gen = [&]() { return static_cast<char>(distribution(GetGenerator())); };
+    std::generate_n(std::back_inserter(buf), n, gen);
+    return buf;
+}
+
 }  // namespace
 
 TEST(Stdlib, test_memcpy)
 {
-    using std::begin;
-    using std::end;
-    typedef std::vector<char> buf_type;
-    buf_type in_buf;
-    static std::uniform_int_distribution<int> distribution(0, 255);
-    static auto const gen = []() {return distribution(generator); };
-    std::generate_n(std::back_inserter(in_buf), 1024, gen);
+    buf_type in_buf = RandomBuf();
     buf_type out_buf(in_buf.size());
     rvv::memcpy(&out_buf[0], &in_buf[0], in_buf.size() * sizeof(buf_type::value_type));
     EXPECT_EQ(in_buf, out_buf);
@@ -35,16 +46,7 @@ TEST(Stdlib, test_memcpy)
 
 TEST(Stdlib, test_memcpy_backward)
 {
-    typedef std::vector<char> buf_type;
-
-    static std::uniform_int_distribution<int> distribution(0, 255);
-    static auto const gen = []() {return distribution(generator); };
-
-    buf_type in_buf;
-    using std::begin;
-    using std::end;
-    std::generate_n(std::back_inserter(in_buf), 1024, gen);
-
+    buf_type in_buf = RandomBuf();
     buf_type out_buf(in_buf.size());
     rvv::memcpy_backward(&out_buf[0], &in_buf[0], in_buf.size() * sizeof(buf_type::value_type));
     EXPECT_EQ(in_buf, out_buf);
@@ -52,39 +54,19 @@ TEST(Stdlib, test_memcpy_backward)
 
 TEST(Stdlib, test_memmove_forward)
 {
-    typedef std::vector<char> buf_type;
-    static std::uniform_int_distribution<int> distribution(0, 255);
-    static auto const gen = []() {return distribution(generator); };
-
-    buf_type ref_buf;
-    using std::begin;
-    using std::end;
-    static size_t const buf_size = 1024;
-    std::generate_n(std::back_inserter(ref_buf), buf_size, gen);
-
+    buf_type ref_buf = RandomBuf();
     buf_type tst_buf = ref_buf;
-
-    std::memmove(&ref_buf[buf_size / 4], &ref_buf[0], buf_size / 2);
-    rvv::memmove(&tst_buf[buf_size / 4], &tst_buf[0], buf_size / 2);
+    std::memmove(&ref_buf[kBufSize / 4], &ref_buf[0], kBufSize / 2);
+    rvv::memmove(&tst_buf[kBufSize / 4], &tst_buf[0], kBufSize / 2);
     EXPECT_EQ(ref_buf, tst_buf);
 }
 
 TEST(Stdlib, test_memmove_backward)
 {
-    static size_t const buf_size = 1024;
-    typedef std::vector<char> buf_type;
-    static std::uniform_int_distribution<int> distribution(0, 255);
-    static auto const gen = []() {return distribution(generator); };
-
-    buf_type ref_buf;
-    using std::begin;
-    using std::end;
-    std::generate_n(std::back_inserter(ref_buf), buf_size, gen);
-
+    buf_type ref_buf = RandomBuf();
     buf_type tst_buf = ref_buf;
-
-    std::memmove(&ref_buf[0], &ref_buf[buf_size / 4], buf_size / 2);
-    rvv::memmove(&tst_buf[0], &tst_buf[buf_size / 4], buf_size / 2);
+    std::memmove(&ref_buf[0], &ref_buf[kBufSize / 4], kBufSize / 2);
+    rvv::memmove(&tst_buf[0], &tst_buf[kBufSize / 4], kBufSize / 2);
     EXPECT_EQ(ref_buf, tst_buf);
 }
 
@@ -93,14 +75,11 @@ class StdlibMemsetTest : public ::testing::TestWithParam<int> {};
 TEST_P(StdlibMemsetTest, test_memset)
 {
     int val = GetParam();
-    typedef std::vector<char> buf_type;
-    buf_type buf(1024);
+    buf_type buf(kBufSize);
     rvv::memset(&buf[0], static_cast<char>(val), buf.size() * sizeof(buf_type::value_type));
-
-    using std::begin;
-    using std::end;
     namespace ph = std::placeholders;
-    EXPECT_TRUE(std::all_of(begin(buf), end(buf), std::bind(std::equal_to<char>(), static_cast<char>(val), ph::_1)));
+    EXPECT_TRUE(std::all_of(buf.begin(), buf.end(),
+        std::bind(std::equal_to<char>(), static_cast<char>(val), ph::_1)));
 }
 
 INSTANTIATE_TEST_SUITE_P(MemsetValues, StdlibMemsetTest, ::testing::Range(0, 256));
